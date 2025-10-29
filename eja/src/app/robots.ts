96e9a5dd8b43954036ce87586615524fb5e25630
cam628 <cfeneon@gmail.com>
2025-10-30 06:08:08 +1100

    chore(seo): align robots and sitemap to production domain editionsjurisafrica.com

--- eja/src/app/robots.ts
+++ eja/src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/private/', '/admin/'],
     },
-    sitemap: 'https://editions-jurisafrica-dju7.vercel.app/sitemap.xml',
+    sitemap: 'https://www.editionsjurisafrica.com/sitemap.xml',
   }
 }
--- eja/src/app/sitemap.ts
+++ eja/src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://editions-jurisafrica-dju7.vercel.app'
+  const baseUrl = 'https://www.editionsjurisafrica.com'
   
   return [
     {
